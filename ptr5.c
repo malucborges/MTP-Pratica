9d04ce7d44e1bf958188bc2ddfad421623861bdd
malucborges <38254808+malucborges@users.noreply.github.com>
2018-05-17 19:42:10 -0300

    Add files via upload

---
+++ ptr5.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+int main()
+{
+	int vetor[1000], i, num, a = 0;
+	unsigned char *p, *q, *r;
+	srand(time(0));
+	printf("\nInsira um numero entre 0 e 255: ");
+	scanf("%d", &num);
+	for(i = 0; i < num; i++)
+		vetor[i] = rand()%num+1;
+	p = (unsigned char*) &vetor;
+	q = (unsigned char*) &vetor[0];
+	printf("\nOs bits nos seguintes enderecos sao iguais a %d:", num);
+	for(i = 0; i < sizeof(vetor); i++)
+	{
+		if(*(p+i) == num)
+		{
+			printf("\n0x%p", &vetor[i]);
+		}
+	}
+	if(i != sizeof(vetor))
+		i++;
+	r = (unsigned char*)&vetor[i];
+	printf("\nEsses bytes estao localizados entre %p ate %p na memoria.", num, &q, &r);
+	return 0;
+}
